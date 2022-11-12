449d85437672ef08aba82d5a35efeba045574395
Callum Taylor <cadtay@hotmail.co.uk>
2022-11-12 10:45:49 +0000

    first commit
    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import '../styles/styles.css'
+import React from 'react'
+import Head from "next/head"
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <Script id="slider"type='text/javascript' src="https://unpkg.com/a11y-slider@0.4.15/dist/a11y-slider.js" defer></Script>
+      <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
